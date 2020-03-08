dd7c945e7b0a0e951effd6bbf98bab6078cd9d2f
Søren Kyndi-Wiuff <skw@lunarway.com>
2020-03-08 17:53:07 +0400

    adds main bindings to start up on docker compose env

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,73 @@ package main
 
 import (
 	"context"
+	"flag"
 	"github.com/go-po/example-teller/internal/api"
 	"github.com/go-po/example-teller/internal/app"
 	"github.com/go-po/example-teller/internal/handlers"
 	"github.com/go-po/po"
 	"log"
-
 	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	flagBroker string
+	flagStore  string
+
+	flagHelp bool
+
+	defaultRabbitUrl = "amqp://teller:teller@localhost:5670"
+	defaultPGUrl     = "postgres://teller:teller@localhost:5430/teller?sslmode=disable"
 )
 
 func main() {
+
+	flag.StringVar(&flagStore, "store", "inmemory", `Set the store
+	inmemory: all data in memory
+	pg: uses a connection url matching the docker-compose.yaml file
+	provide your own connection url
+`)
+	flag.StringVar(&flagBroker, "broker", "channels", `Set the broker
+	channels: use the channels broker
+	rabbit: uses the RabbitMQ matching the docker-compose.yaml file
+	provide your own RabbitMQ url
+`)
+
+	flag.BoolVar(&flagHelp, "help", false, "Print this help message")
+
+	flag.Parse()
+
+	if flagHelp {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	var broker po.Option
+	if flagBroker == "rabbit" {
+		broker = po.WithBrokerRabbit(defaultRabbitUrl, "teller", "app")
+	} else if flagBroker != "channels" && strings.TrimSpace(flagBroker) != "" {
+		broker = po.WithBrokerRabbit(flagBroker, "teller", "app")
+	} else {
+		broker = po.WithBrokerChannel()
+	}
+
+	var store po.Option
+	if flagStore == "pg" {
+		store = po.WithStorePGUrl(defaultPGUrl)
+	} else if flagStore != "inmemory" && strings.TrimSpace(flagStore) != "" {
+		store = po.WithStorePGUrl(flagStore)
+	} else {
+		store = po.WithStoreInMemory()
+	}
+
+	log.Printf("Starting Teller with store [%s] and broker [%s]", flagStore, flagBroker)
+
 	rootCtx := context.Background()
 
 	// initialize PO
-	dao, err := po.NewFromOptions(
-		po.WithBrokerChannel(),
-		po.WithStoreInMemory(),
-	)
+	dao, err := po.NewFromOptions(broker, store)
 	if err != nil {
 		log.Fatalf("failed initialize po: %s", err)
 	}
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/go-po/example-teller
 go 1.14
 
 require (
-	github.com/go-po/po v0.0.0-20200306125643-ef644ee8a840
+	github.com/go-po/po v0.0.0-20200306132816-76d34669b097
 	github.com/jteeuwen/go-bindata v3.0.7+incompatible // indirect
 	github.com/kyleconroy/sqlc v1.0.0 // indirect
 	github.com/stretchr/testify v1.4.0
